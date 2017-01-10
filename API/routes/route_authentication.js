875b508a2b36a0021737a41f9d4cf2296a5189d7
Christopher Georget <chrigeo76@gmail.com>
2017-01-10 17:49:51 +0100

    Mise à jour doc register

--- API/routes/route_authentication.js
+++ API/routes/route_authentication.js
@@ -10,13 +10,17 @@ var router = express.Router();
 /* POST Register new user
  * Consumes JSON : { login, password, password_confirmation, user_name, 
  *                   user_surname, user_phone, user_birthdate }
- *  login : Pseudo du nouvel utilisateur
- *  password : Mot de passe du nouvel utilisateur
+ *  login                 : Pseudo du nouvel utilisateur
+ *  password              : Mot de passe du nouvel utilisateur
  *  password_confirmation : Confirmation du mot de passe du nouvel utilisateur
- *  user_name: Nom de l'utilisateur
- *  user_surname : Prénom de l'utilisateur
- *  user_phone: Numéro de téléphone de l'utilisateur
- *  user_birthdate: Date de naissance de l'utilisateur
+ *  user_name             : Nom de l'utilisateur
+ *  user_surname          : Prénom de l'utilisateur
+ *  user_phone            : Numéro de téléphone de l'utilisateur
+ *  user_birthdate        : Date de naissance de l'utilisateur
+ * Returns:
+ *  400 Bad Request       : password et password_confirmation différents
+ *  500 Server Error      : Erreur lors de l'enregistrement dans la base
+ *  200 OK                : Register s'est bien passé
  */
 router.post('/register', function(req, res) {
   // Si le mot de passe et la confirmation sont différentes, c'est une 400 Bad Request
@@ -27,22 +31,21 @@ router.post('/register', function(req, res) {
   bcrypt.hash(req.body.password, 10, function(err, hash) {
     // Initialisation des valeurs à rentrer dans la BDD
     var data = {
-      utilisateur_nom: req.body.user_name,
-      utilisateur_prenom: req.body.user_surname,
-      utilisateur_portable: req.body.user_phone,
-      utilisateur_date_naissance: req.body.user_birthdate,
-      utilisateur_pseudo: req.body.login,
-      utilisateur_date_creation: new Date(),
-      utilisateur_date_modification: new Date(),
+      utilisateur_nom:                     req.body.user_name,
+      utilisateur_prenom:                  req.body.user_surname,
+      utilisateur_portable:                req.body.user_phone,
+      utilisateur_date_naissance:          req.body.user_birthdate,
+      utilisateur_pseudo:                  req.body.login,
+      utilisateur_date_creation:           new Date(),
+      utilisateur_date_modification:       new Date(),
       utilisateur_date_derniere_connexion: new Date(),
-      utilisateur_token: '000',
-      utilisateur_mot_de_passe: hash
+      utilisateur_token:                   '000',
+      utilisateur_mot_de_passe:            hash
     };
 
     // On récupère une connexion du pool et on exécute un INSERT
     pool.query('INSERT INTO utilisateur SET ?', data, function(error, result) {
       if (error) {
-        console.log(error); 
         res.sendStatus(500); 
       }
 
