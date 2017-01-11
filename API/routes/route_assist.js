85f0b3928b25150707d4d2e5759a0c6412e26584
ATENSI <alexandre.telal@ecole.ensicaen.fr>
2017-01-11 13:24:15 +0100

    [FIX] Ajout du token

--- API/routes/route_assist.js
+++ API/routes/route_assist.js
@@ -6,9 +6,10 @@ var router = express.Router();
 
 // Routes configurations
 
-/* POST Register new user
+/* POST Create an assist demand
  * Consumes JSON : { utilisateur_id_2, trajet_longitude_arrivee, trajet_latitude_arrivee,
  *                   trajet_public, utilisateur_id }
+ *  token                      : Token
  *  utilisateur_id_2           : Identifiant de l'utilsateur portant assistance
  *  assistance_longitude       : Longitude du point d'assistance
  *  assistance_latitude        : Latitude du point d'assistance
@@ -19,29 +20,27 @@ var router = express.Router();
  *  200 OK                : Register s'est bien passé
  */
 router.post('/create', function(req, res) {
-  // Si le mot de passe et la confirmation sont différentes, c'est une 400 Bad Request
-  if (req.body.password !== req.body.password_confirmation)
-    res.sendStatus(400);
+    loginUtils.checkConnection(req.body.token).then(function(logged){
+      if(logged){
+          var data = {
+            utilisateur_id_2:           req.body.utilisateur_id_2,
+            assistance_longitude:       req.body.assistance_longitude,
+            assistance_latitude:        req.body.assistance_latitude,
+            utilisateur_utilisateur_id: req.body.utilisateur_id
+          };
 
-  // Chiffrage du mot de passe
-  bcrypt.hash(req.body.password, 10, function(err, hash) {
-    // Initialisation des valeurs à rentrer dans la BDD
-    var data = {
-      utilisateur_id_2:           req.body.utilisateur_id_2,
-      assistance_longitude:       req.body.assistance_longitude,
-      assistance_latitude:        req.body.assistance_latitude,
-      utilisateur_utilisateur_id: req.body.utilisateur_id
-    };
+          // On récupère une connexion du pool et on exécute un INSERT
+          pool.query('INSERT INTO assistance SET ?', data, function(error, result) {
+            if (error) {
+              res.sendStatus(500);
+            }
 
-    // On récupère une connexion du pool et on exécute un INSERT
-    pool.query('INSERT INTO assistance SET ?', data, function(error, result) {
-      if (error) {
-        res.sendStatus(500);
-      }
-
-      res.sendStatus(200);
-    });
-
+            res.sendStatus(200);
+          });
+    }
+    else {
+          res.sendStatus(403);
+    }
   });
 });
 
